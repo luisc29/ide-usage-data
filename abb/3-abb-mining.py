60773f80b6854f799a9b94e63fa7564ef9b95d4d
Luis Carlos Cruz <luiscruz.2908@gmail.com>
2016-06-08 14:00:29 -0500

    clustering with affinity propagation

--- abb/3-abb-mining.py
+++ abb/3-abb-mining.py
@@ -1,23 +1,19 @@
 from __future__ import division
 
-import matplotlib as mp
-import matplotlib.pyplot as plt
 import numpy as np
 import random
 import scipy
 from pandas import *
 from sklearn.cluster import KMeans
+from sklearn.cluster import AffinityPropagation
 from collections import Counter
-from sklearn import metrics
 from sklearn.ensemble import ExtraTreesClassifier
 
 
 PATH_TO_RESULT_MAIN = "/home/luis/abb/"
 PATH_TS_FILE = "/home/luis/abb/ts_abb.csv"  
 TIME_SERIES_NAMES = ['edition', 'text_nav', 'high_nav', 'file', 'refactoring',
-                     'debug']
-#TIME_SERIES_NAMES = ['clean_build', 'debug', 'tools', 'control', 'testing', 'search']
-
+                     'debug', 'clean_build', 'tools', 'control', 'testing', 'search']
 
 def calc_proportions(data):
     """
@@ -42,94 +38,30 @@ def calc_proportions(data):
     return data
 
             
-def initialize_kmeanspp(X, K):
+def initialize_kmeanspp(x, k):
     '''
     Initializes the centroids for the given vector of values
     '''
     random.seed(29)
-    C = [X[0]]
-    for k in range(1, K):
-        D2 = scipy.array([min([scipy.inner(c-x,c-x) for c in C]) for x in X])
-        probs = D2/D2.sum()
+    c = [x[0]]
+    for k in range(1, k):
+        d = scipy.array([min([scipy.inner(c-x,c-x) for c in c]) for x in x])
+        probs = d/d.sum()
         cumprobs = probs.cumsum()
         r = random.random()
         for j,p in enumerate(cumprobs):
             if r < p:
                 i = j
                 break
-        C.append(X[i])
-    return C
+        c.append(x[i])
+    return c
 
 
-def clustering_by_metrics(data):
-    '''
-    Finds clusters according to the metrics per session
-    '''
-    data_res = DataFrame()
-
-    users = data['user'].unique()
-    n_k = len(users)
-
-    data_res['emin'] = data['emin']
-    data_res['smin'] = data['smin']
-    data_res['eratio'] = data['eratio']
-
-    # fitting the model
-    print('Fitting a k-means model')
-    print(' Number of clusters: ' + str(n_k))
-    km = KMeans(n_clusters=n_k)
-    km.fit(data_res)
-
-    print('Predicting cluster for every session')
-    predictions = km.predict(data_res)
-    data['prediction'] = predictions
-
-    # calculating error
-    print('Calculating error')
-    errors = []
-    common_prediction = []
-    n_sessions = []
-    for u in users:
-        data_user = data[data['user'] == u]
-        user_predictions = np.asarray(data_user['prediction'])
-        most_common_value = Counter(user_predictions).most_common(1)
-        user_pred = most_common_value[0][0]
-        error = most_common_value[0][1] / len(data_user)
-        errors.append(error)
-        common_prediction.append(user_pred)
-        n_sessions.append(len(data_user))
-
-    error_prediction = DataFrame()
-    error_prediction['user'] = users
-    error_prediction['common_prediction'] = common_prediction
-    error_prediction['error'] = errors
-    error_prediction['n_sessions'] = n_sessions
-    print error_prediction
-    print('average error: ' + str(np.mean(errors)))
-    return data
-
-
-def clustering_by_proportions(data):
+def clustering_by_proportions_kmeans(data_res, data, users):
     """
     Finds clusters by the proportion of events
     """
-    data_res = DataFrame()
-
-    users = data['user'].unique()
     n_k = len(users)
-
-    # prepare the dataset
-    for ts in TIME_SERIES_NAMES:
-        data_res['prop_' + ts] = data['prop_' + ts]
-
-    # feature selection
-    model = ExtraTreesClassifier()
-    model.fit(data_res, data['user'])
-    print('Feature importances')
-    feature_importances = DataFrame()
-    feature_importances['feature'] = TIME_SERIES_NAMES
-    feature_importances['importance'] = model.feature_importances_
-    print feature_importances
 
     # fitting the model
     print('Fitting a k-means model')
@@ -214,21 +146,75 @@ def clustering_by_proportions(data):
     return data
 
 
+def clustering_by_proportions_affinity(data_res, data, users):
+    af = AffinityPropagation()
+    af.fit(data_res)
+    centers_locations = af.cluster_centers_
+    predictions = af.predict(data_res)
+    data['prediction'] = predictions
+    centers = DataFrame(centers_locations, columns=TIME_SERIES_NAMES)
+
+    n_sessions = []
+    users_by_cluster = [0]*len(centers_locations)
+    n_predictions_by_cluster = [0]*len(centers_locations)
+    user_list_by_cluster = ['']*len(centers_locations)
+    c = 65
+    for u in users:
+        data_user = data[data['user'] == u]
+        user_predictions = np.asarray(data_user['prediction'])
+
+        # counts the number of times a session was predicted in some cluster
+        for i in user_predictions:
+            n_predictions_by_cluster[i] += 1
+
+        unique_predictions = set(user_predictions)
+        for i in unique_predictions:
+            users_by_cluster[i] += 1
+            user_list_by_cluster[i] += chr(c)
+
+        # calculate the error using the top 1 prediction
+        n_sessions.append(len(data_user))
+
+        c += 1
+
+    centers['n_predictions'] = n_predictions_by_cluster
+    centers['n_users'] = users_by_cluster
+    centers['users'] = user_list_by_cluster
+
+    centers.to_csv(PATH_TO_RESULT_MAIN + 'af_clusters.csv')
+
+
 def clustering(data):
     """
-    Perform clustering of sessions
+    Performs clustering of sessions. Prepares the data-
     """
-
+    original = data
     # remove data from users with little information
     data_aggregated = DataFrame({'count':data.groupby(["user"]).size()}).reset_index()
     list_users = np.asarray(data_aggregated[data_aggregated['count'] >= 3]['user'])
     data = data[data['user'].isin(list_users)]
 
+    # prepare the dataset
+    data_res = DataFrame()
+    for ts in TIME_SERIES_NAMES:
+        data_res['prop_' + ts] = data['prop_' + ts]
+
+    # feature selection
+    model = ExtraTreesClassifier()
+    model.fit(data_res, data['user'])
+    print('Feature importances')
+    feature_importances = DataFrame()
+    feature_importances['feature'] = TIME_SERIES_NAMES
+    feature_importances['importance'] = model.feature_importances_
+    print feature_importances
+
     # implement clustering
-    print('\n\n     Clustering by proportions of events')
-    clustering_by_proportions(data)
-    print('\n\n     Clustering by metrics')
-    #clustering_by_metrics(data)
+    print('\n\n     Clustering with k-means')
+    users = data['user']
+    users = users.unique()
+    #clustering_by_proportions_kmeans(data_res, data, users)
+    print('\n\n     Clustering with affinity propagation')
+    clustering_by_proportions_affinity(data_res, data, users)
 
 
 def calc_metrics(data):
@@ -272,7 +258,5 @@ if __name__ == "__main__":
     sessions = pandas.read_csv(PATH_TS_FILE, index_col=None, header=0)
     sessions = calc_proportions(sessions)
     sessions = calc_metrics(sessions)
-    #sessions = sessions.dropna()
 
     sessions = clustering(sessions)
-    #sessions.to_csv(PATH_TO_RESULT_MAIN + 'after_prop.csv', index=False)
